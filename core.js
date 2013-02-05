f236e6cfb0597bbecb83d2e63e89fb0f9dfdf6ba
Phil Corbett <corbett.phil@gmail.com>
2013-02-04 20:35:28 -0500

    Hello World plus console for files

--- core.js
+++ core.js
@@ -1,12 +1,19 @@
-var fs = require('fs')
+var http = require('http'),
+	fs = require('fs'),
 	securityCamPath = 'C:\\Storage\\SecurityCam\\';
 
-fs.readdir(securityCamPath, function (err, files) {
-	if (err) throw err;
-	for(var i = 0; i < files.length; i++) {
-		var f = files[i];
-		if(f.indexOf('.jpg') != -1) {
-			console.log(files[i]);	
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  
+  fs.readdir(securityCamPath, function (err, files) {
+		if (err) throw err;
+		for(var i = 0; i < files.length; i++) {
+			var f = files[i];
+			if(f.indexOf('.jpg') != -1) {
+				console.log(files[i]);	
+			}
 		}
-	}
-})
+	});
+	
+  response.end('Hello World\n');
+}).listen(8124);
