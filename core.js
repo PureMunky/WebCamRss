8f6c50be8304d7a72c215741cecbe19ac515e85e
Phil Corbett <corbett.phil@gmail.com>
2013-02-04 20:20:55 -0500

    add loop through security cam files

--- core.js
+++ core.js
@@ -1,6 +1,14 @@
-var fs = require('fs');
+var fs = require('fs')
+	securityCamPath = 'C:\\Storage\\SecurityCam\\';
 
 fs.appendFile('message.txt', 'data to append', function (err) {
   if (err) throw err;
   console.log('The "data to append" was appended to file!');
 });
+
+fs.readdir(securityCamPath, function (err, files) {
+	if (err) throw err;
+	for(var i = 0; i < files.length; i++) {
+		console.log(files[i]);
+	}
+})
