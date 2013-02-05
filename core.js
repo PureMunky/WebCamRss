0e5e6b041c177fad77ebd0f3043cfc5d403f6d7e
Phil Corbett <corbett.phil@gmail.com>
2013-02-04 20:24:14 -0500

    update loop to only display jpg files

--- core.js
+++ core.js
@@ -1,14 +1,12 @@
 var fs = require('fs')
 	securityCamPath = 'C:\\Storage\\SecurityCam\\';
 
-fs.appendFile('message.txt', 'data to append', function (err) {
-  if (err) throw err;
-  console.log('The "data to append" was appended to file!');
-});
-
 fs.readdir(securityCamPath, function (err, files) {
 	if (err) throw err;
 	for(var i = 0; i < files.length; i++) {
-		console.log(files[i]);
+		var f = files[i];
+		if(f.indexOf('.jpg') != -1) {
+			console.log(files[i]);	
+		}
 	}
 })
